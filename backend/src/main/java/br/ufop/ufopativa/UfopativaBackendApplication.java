c04f9c1097f55a2f6e1e481585ca27abd1a6177a
Mateus Lana <mateus_lana7@hotmail.com>
2020-06-08 13:44:41 -0300

    Add e config arquivos para deploy no gcloud

--- backend/src/main/java/br/ufop/ufopativa/UfopativaBackendApplication.java
+++ backend/src/main/java/br/ufop/ufopativa/UfopativaBackendApplication.java
@@ -1,11 +1,20 @@
 package br.ufop.ufopativa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufop.ufopativa.services.PopulateDatabaseService;
 
 @SpringBootApplication
+@RestController
 public class UfopativaBackendApplication implements CommandLineRunner{
+	
+	@Autowired
+	private PopulateDatabaseService pdbService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UfopativaBackendApplication.class, args);
@@ -13,7 +22,19 @@ public class UfopativaBackendApplication implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception{
+		/**
+		 * CHAMADA DO METODO QUE FAZ A LEITURA DOS CURRICULOS EM XML,
+		 * MAPEIA OS DADOS PARA A ESTRUTURA DO BANCO E POPULA O MESMO
+		 * */
+		boolean populateDb = false;
+		if(populateDb)	
+			pdbService.populate();
 		System.out.println("\nUFOP-ATIVA BACKEND RODANDO...\n");
 	}
+
+	// @GetMapping("/")
+	// public String welcome(){
+	// 	return "Bem Vindo ao UFOP Ativa";
+	// }
 
 }
